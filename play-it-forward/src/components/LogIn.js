f967002d202acd2ba918fdd15bffc092aad93d5a
Genevieve McAllister <kj.genevieve@gmail.com>
2019-11-23 13:18:04 -0800

    Added routes and basic display data

---
+++ play-it-forward/src/components/LogIn.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+export default class LogIn extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Log In</h1>
+      </div>
+    );
+  }
+}
