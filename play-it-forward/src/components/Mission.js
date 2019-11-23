4e28bbbb7d08dfb908f02542737af8efbbeee6e7
Taljjaa <jenken131@gmail.com>
2019-11-23 13:11:25 -0800

    finalized mission statement

--- play-it-forward/src/components/Mission.js
+++ play-it-forward/src/components/Mission.js
@@ -4,7 +4,9 @@ function Mission() {
    return (
       <p>
          Play It Forward, is a web app aimed to gamify volunteering. Log your
-         nonprofit hours and gain experience, points, and rewards. 
+         nonprofit hours and gain experience, points, and rewards.
       </p>
    );
 }
+
+export default Mission;
