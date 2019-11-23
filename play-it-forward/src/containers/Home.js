3569327301b196e11975a3614cb5dd4a0114c928
Genevieve McAllister <kj.genevieve@gmail.com>
2019-11-23 13:37:06 -0800

    Cards show

--- play-it-forward/src/containers/Home.js
+++ play-it-forward/src/containers/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import Mission from '../components/Mission';
+import Search from '../components/Search';
+import CardDisplay from './CardDisplay';
 export default class Home extends Component {
   
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <Mission />
+        <Search />
+        <CardDisplay />
+        <CardDisplay />
       </div>
     );
   }
