72bd83aefd77539783502208f82974a46f374d0c
Taljjaa <jenken131@gmail.com>
2019-11-23 13:01:22 -0800

    finished mission statement

--- play-it-forward/src/components/Mission.js
+++ play-it-forward/src/components/Mission.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 function Mission() {
-   return <p>Hello</p>;
+   return (
+      <p>
+         Play It Forward, is a web app aimed to gamify volunteering. Log your
+         nonprofit hours and gain experience, points, and rewards. 
+      </p>
+   );
 }
