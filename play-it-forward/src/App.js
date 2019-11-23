877265e63214847a9824cd8047e720ebcc6502a3
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-11-23 15:33:53 -0800

    Add a contact form + footer

--- play-it-forward/src/App.js
+++ play-it-forward/src/App.js
@@ -3,21 +3,26 @@ import Home from './containers/Home.js';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/NavBar.js';
+import Footer from './components/Footer.js';
 import LogIn from './components/LogIn.js';
 import SignUp from './components/SignUp.js';
 import About from './components/About.js';
+import Contact from './components/Contact.js';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/logIn' component={LogIn} />
-          <Route path='/signUp' component={SignUp} />
-          <Route path='/about' component={About} />
-        </Switch>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/logIn' component={LogIn} />
+            <Route path='/signUp' component={SignUp} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
