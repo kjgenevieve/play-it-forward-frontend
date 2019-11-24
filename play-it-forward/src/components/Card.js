a1576284df1a06ec61f414be57d6fc96df676e2f
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-11-23 16:16:16 -0800

    cards

--- play-it-forward/src/components/Card.js
+++ play-it-forward/src/components/Card.js
@@ -3,24 +3,57 @@ export default class Card extends Component {
   componentDidMount() {
     console.log(this.props)
   }
-  
+
   renderData = () => {
     if (this.props.event) {
       return(
         <div>
-          <h3>{this.props.event.name}</h3>
-          <h4>{this.props.event.description}</h4>
-          <h4>{this.props.event.date}</h4>
-          <h4>{this.props.event.location}</h4>
+          <div class="row">
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">{this.props.event.name}</h5>
+                    <p class="card-text">{this.props.event.description}</p>
+                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">{this.props.event.name}</h5>
+                    <p class="card-text">{this.props.event.description}</p>
+                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       )
     } else if (this.props.nonProfit) {
       return(
         <div>
-          <h3>{this.props.nonProfit.name}</h3>
-          <h4>{this.props.nonProfit.description}</h4>
-          <h4>{this.props.nonProfit.mission}</h4>
-          <h4>{this.props.nonProfit.address}</h4>
+          <div class="row">
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">{this.props.event.name}</h5>
+                    <p class="card-text">{this.props.event.description}</p>
+                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">{this.props.nonProfit.name}</h5>
+                    <p class="card-text">{this.props.nonProfit.description}</p>
+                    <h5 class="btn btn-primary">{this.props.nonProfit.mission}</h5>
+                    <h5 class="btn btn-primary">{this.props.nonProfit.adress}</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
       )
     }
