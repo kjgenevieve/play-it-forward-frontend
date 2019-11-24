a3c4241c3156144c8367da4db14145ae94cd0b3c
Sabrina Mianne <sabrina.epicodus@gmail.com>
2019-11-23 16:55:03 -0800

    update cards

--- play-it-forward/src/components/Card.js
+++ play-it-forward/src/components/Card.js
@@ -8,53 +8,58 @@ export default class Card extends Component {
     if (this.props.event) {
       return(
         <div>
-          <div class="row">
-              <div class="col-sm-6">
-                <div class="card">
+          <div className="row" style={{marginLeft:"auto", marginRight:"auto"}}>
+            <div className="col" >
+                <div class="card" style={{ margin:1+"em"}}>
                   <div class="card-body">
                     <h5 class="card-title">{this.props.event.name}</h5>
                     <p class="card-text">{this.props.event.description}</p>
-                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+                    <h5 class="card-link">{this.props.event.date} | {this.props.event.location}</h5>
                   </div>
                 </div>
               </div>
-              <div class="col-sm-6">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title">{this.props.event.name}</h5>
-                    <p class="card-text">{this.props.event.description}</p>
-                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+              <div className="col">
+                <div class="card" style={{ margin:1+"em"}}>
+                  <div class="card" style={{ margin:1+"em"}}>
+                    <div class="card-body">
+                      <h5 class="card-title">{this.props.event.name}</h5>
+                      <p class="card-text">{this.props.event.description}</p>
+                      <h5 class="card-link">{this.props.event.date} | {this.props.event.location}</h5>
+                    </div>
                   </div>
-                </div>
               </div>
             </div>
         </div>
+      </div>
       )
     } else if (this.props.nonProfit) {
       return(
         <div>
-          <div class="row">
-              <div class="col-sm-6">
-                <div class="card">
+          <div className="row" style={{marginLeft:"auto", marginRight:"auto"}}>
+            <div className="col" >
+                <div class="card" style={{ margin:1+"em"}}>
                   <div class="card-body">
                     <h5 class="card-title">{this.props.event.name}</h5>
                     <p class="card-text">{this.props.event.description}</p>
-                    <h5 class="btn btn-primary">{this.props.event.date} | {this.props.event.location} </h5>
+                    <h5 class="card-link">{this.props.event.date} | {this.props.event.location}</h5>
                   </div>
                 </div>
               </div>
-              <div class="col-sm-6">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title">{this.props.nonProfit.name}</h5>
-                    <p class="card-text">{this.props.nonProfit.description}</p>
-                    <h5 class="btn btn-primary">{this.props.nonProfit.mission}</h5>
-                    <h5 class="btn btn-primary">{this.props.nonProfit.adress}</h5>
+              <div className="col">
+                <div class="card" style={{ margin:1+"em"}}>
+                  <div class="card" style={{ margin:1+"em"}}>
+                    <div class="card-body">
+                      <h5 class="card-title">{this.props.event.name}</h5>
+                      <p class="card-text">{this.props.event.description}</p>
+                      <h5 class="card-link">{this.props.nonProfit.mission}</h5>
+                      <p class="card-link">{this.props.nonProfit.adress}</p>
+                    </div>
                   </div>
-                </div>
               </div>
-            </div>
-        </div>
+          </div>
+      </div>
+    </div>
+
       )
     }
   }
